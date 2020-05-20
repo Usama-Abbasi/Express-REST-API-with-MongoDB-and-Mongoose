6ff24577dbecb2d0da4935be73e8c91717d448e8
usama <usama999abbasi@gmail.com>
2020-05-20 20:56:47 +0500

    Express REST API with MongoDB and Mongoose Part 1"

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose=require('mongoose');
+const Dishes=require('./models/dishes');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,6 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,6 +29,12 @@ app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+
+const url= 'mongodb://localhost:27017/conFusion';
+const connect=mongoose.connect(url);
+connect.then((db)=>{
+   console.log('Connect to the Server Correctly...');
+},(err)=>{console.log(err)});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
