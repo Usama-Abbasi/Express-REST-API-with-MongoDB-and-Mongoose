4bbc4af99b994d92a91055a128190e47e1b151b8
usama <usama999abbasi@gmail.com>
2020-05-22 17:32:25 +0500

    Passwort

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var passport = require('passport');
+var authenticate = require('./authenticate');
 var app = express();
 
 // view engine setup
@@ -31,27 +33,21 @@ app.use(session({
   resave:false,
   store:new FIleStore()
 }));
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 function auth (req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-if(!req.session.user) {
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
-    return next(err);
-}
-else {
-  if (req.session.user === 'authenticated') {
-    next();
+    next(err);
   }
   else {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
+        next();
   }
-}
 }
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
--- models/user.js
+++ models/user.js
@@ -1,20 +1,11 @@
 var mongoose=require('mongoose');
 var Schema=mongoose.Schema;
-
+var passLocalMongoose=require('passport-local-mongoose');
 var User=new Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-
-    },
-    password:{
-        type:String,
-        required:true
-    },
     admin:{
         type:Boolean,
         default:false
     }
 });
+User.plugin(passLocalMongoose);
 module.exports=mongoose.model('User',User);
